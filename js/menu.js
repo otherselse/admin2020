acc79fe941ca7e2f611e2049c3acd3a3e20901ea
xyy <otherselse@163.com>
2020-08-04 21:03:28 +0800

    start

--- js/menu.js
+++ js/menu.js
@@ -59,12 +59,16 @@ $(function(){
 		,{"title":"审批管理",
 		 "menuList":[
 			 	{"bigM":"角色管理类1","smallM":[{"mtitle":"角色管理类small1","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}
+			 	,{"bigM":"角色管理类2","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}	
+			 	,{"bigM":"角色管理类3","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}
+			 	,{"bigM":"角色管理类3","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}	
 			]
 		}
 		,{"title":"审批管理",
 		 "menuList":[
 			 	{"bigM":"角色管理类1","smallM":[{"mtitle":"角色管理类small1","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}
 			 	,{"bigM":"角色管理类2","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}	
+			 	,{"bigM":"角色管理类3","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}
 			 	,{"bigM":"角色管理类3","smallM":[{"mtitle":"角色管理类small2","href":"../index/index.html"},{"mtitle":"角色管理类small2","href":"../index/index.html"}]}	
 			 			
 
@@ -105,26 +109,58 @@ $(function(){
 	}
 	$(".menuConfig").html(_menuStr)
 
-
+	if(!IsPC()){ //手机端
+		var logo=$(".logo").clone(true);
+		var topRight=$(".topRight").clone(true);
+		$(".m_top").append(logo).append(topRight);
+		$("body").addClass("m_status")
+	}
 
 	$("body").on("mouseover",".menuhook",function(){
-		var _ww=$(window).width()/2;
-		var _myL=$(this).offset().left;
+		if(IsPC()){
+			var _ww=$(window).width()/2;
+			var _myL=$(this).offset().left;
+			var _index=$(this).parents(".menuConfig").find(".menuhook").index($(this));
+			//console.log(_index)
 
-
-		if($(this).attr("datachild")>="4"){
-			$(this).find(".mDetail").css("left",_ww-_myL-$(this).find(".mDetailCont").outerWidth(true)/2);
-		}else if($(this).attr("datachild")=="2"||$(this).attr("datachild")=="3"){
-			if(_myL>_ww){  //在屏幕右侧
-				_w=$(this).find(".mDetailCont").outerWidth(true)-$(this).outerWidth(true);
-			}else{  //在屏幕左侧
-				_w=0;
+			if($(this).attr("datachild")>="4"){
+				if(_index>7){
+						$(this).find(".mDetail").css("left",-($(this).find(".mDetailCont").outerWidth(true)-$(this).outerWidth(true)))
+				}else{
+					$(this).find(".mDetail").css("left",_ww-_myL-$(this).find(".mDetailCont").outerWidth(true)/2);
+				}
+				
+			}else if($(this).attr("datachild")=="2"||$(this).attr("datachild")=="3"){
+				if(_myL>_ww){  //在屏幕右侧
+					_w=$(this).find(".mDetailCont").outerWidth(true)-$(this).outerWidth(true);
+				}else{  //在屏幕左侧
+					_w=0;
+				}
+				
+				$(this).find(".mDetail").css("left",-_w)
 			}
-			
-			$(this).find(".mDetail").css("left",-_w)
+		}else{
+			$(this).find(".mDetail").css("left",0)
 		}
+		
 		$(this).find(".mDetail").show();
 	}).on("mouseout",".menuhook",function(){
 		$(this).find(".mDetail").hide();
 	})
 })
+
+
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
